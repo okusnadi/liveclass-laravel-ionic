30fabcd01a306fc152f5d97450d14386d3f72848
unknown <hernane.analista@gmail.com>
2015-10-20 21:19:42 -0200

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    private $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+    public function index()
+    {
+        return $this->post->all();
+    }
+}
